64f2c1bc6fec98579db24a8996533e6f5cd56c9d
Vaibhav Singh <singhrisu2000@gmail.com>
2025-08-26 16:47:03 +0530

    Improved frontend: added input field and custom message sending

---
+++ frontend/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { connect, sendMsg } from './api';
+import Header from './components/Header';
+import ChatHistory from './components/ChatHistory';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatHistory: []
+    }
+  }
+
+  send() {
+    console.log("Hello");
+    sendMsg("Hello...")
+  }
+
+  componentDidMount() {
+    connect((msg) => {
+      console.log("New message")
+      this.setState(prevState => ({
+        chatHistory: [...this.state.chatHistory, msg]
+      }))
+      console.log(this.state)
+    });
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Header />
+        <ChatHistory chatHistory ={this.state.chatHistory} />
+        <button onClick={() => this.send()}>Hit</button>
+      </div>
+    );
+  }
+}
+
+
+export default App;
---
+++ frontend/src/api/index.js
@@ -0,0 +1,34 @@
+// api/index.js
+
+var socket = new WebSocket("ws://localhost:8080/ws")
+
+let connect = cb => {
+    console.log("Attempting connection....");
+
+    socket.onopen = () => {
+        console.log("Successfully Connected!!!!");
+    };
+
+    socket.onmessage = msg => {
+        console.log(msg);
+        cb(msg);
+    };
+
+    socket.onclose = event => {
+        console.log("Socket closed connection: ", event);
+    };
+
+    socket.onerror = error => {
+        console.log("Socket error: ", error);
+    };
+    
+    
+};
+
+let sendMsg = msg => {
+    console.log("Sending msg: ", msg);
+    socket.send(msg);
+};
+
+
+export { connect, sendMsg };
