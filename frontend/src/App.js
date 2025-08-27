88e53870f1386c6cc816db79cc6d3b98636dc630
Vaibhav Singh <singhrisu2000@gmail.com>
2025-08-27 13:38:40 +0530

    Improved frontend part and added custom message feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { connect, sendMsg } from './api';
 import Header from './components/Header';
 import ChatHistory from './components/ChatHistory';
+import ChatInput from './components/ChatInput';
 
 
 class App extends Component {
@@ -14,9 +15,11 @@ class App extends Component {
     }
   }
 
-  send() {
-    console.log("Hello");
-    sendMsg("Hello...")
+  send(event) {
+    if (event.keyCode === 13) {
+      sendMsg(event.target.value);
+      event.target.value = "";
+    }
   }
 
   componentDidMount() {
@@ -34,7 +37,7 @@ class App extends Component {
       <div className='App'>
         <Header />
         <ChatHistory chatHistory ={this.state.chatHistory} />
-        <button onClick={() => this.send()}>Hit</button>
+        <ChatInput send={this.send} />
       </div>
     );
   }
